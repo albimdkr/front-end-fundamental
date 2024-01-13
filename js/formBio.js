2b22d8381f08f67c0556c197de8b57a981fbd7d6
albimdkr <albimudakar04@gmail.com>
2024-01-13 10:03:30 +0700

    feat: adding js for formBio

---
+++ js/formBio.js
@@ -0,0 +1,24 @@
+const storageKey = 'STORAGE_KEY';
+const submitAction = document.getElementById('form-data-user');
+
+// check storage
+function checkForStorage() {
+  return typeof Storage !== 'undefined';
+}
+
+// putUserList dari local storage
+function putUserList(data) {
+  if (checkForStorage()) {
+    let userData = [];
+    if (localStorage.getItem(storageKey) !== null) {
+      userData = JSON.parse(localStorage.getItem(storageKey));
+    }
+
+    userData.unshift(data);
+    if (userData.length > 5) {
+      userData.pop();
+    }
+
+    localStorage.setItem(storageKey, JSON.stringify(userData));
+  }
+}
