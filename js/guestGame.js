2549e3e507a3d2883d63e3b550ea162cc7f93da6
albimdkr <albimudakar04@gmail.com>
2024-01-23 12:03:30 +0700

    feat: adding initialization DOM

---
+++ js/guestGame.js
@@ -0,0 +1,27 @@
+//inisialisasi variabel untuk menampung elemen dokumen
+const localTotalVictoryField = document.getElementById(
+  'local-total-victory-field'
+);
+const localMaximumAttemptField = document.getElementById(
+  'local-maximum-attempt-field'
+);
+const destroyDataButton = document.getElementById('destroy-data-button');
+const playButton = document.getElementById('play-button');
+const beforeGameDisplay = document.getElementById('before-game-display');
+const duringGameDisplay = document.getElementById('during-game-display');
+const afterGameDisplay = document.getElementById('after-game-display');
+const answerButton1 = document.getElementById('answer-1-button');
+const answerButton2 = document.getElementById('answer-2-button');
+const answerButton3 = document.getElementById('answer-3-button');
+const sessionUserAnswerField = document.getElementById(
+  'session-user-answer-field'
+);
+const sessionUserWrongAnswerField = document.getElementById(
+  'session-user-wrong-answer-field'
+);
+const sessionTrueAnswerField = document.getElementById(
+  'session-true-answer-field'
+);
+const sessionUserAttemptsField = document.getElementById(
+  'session-user-attempts-amount-field'
+);
